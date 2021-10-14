036fb7211d4e1f12651be08efa26588f53dc0011
Farida <faridamus@protonmail.com>
2021-10-14 03:57:58 +0000

    Modify packing list

--- packing-list.js
+++ packing-list.js
@@ -1,13 +1,12 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
-
-for (let i = 0; i < packingList.length; i++) {
+//console.log("Kitchen stuff to pack:");
+//for (let i = 0; i < packingList.length; i++) {
+//  console.log(packingList[i]);
+//}
+console.log("Kitchen staff to pack:");
+let i = 0;
+while (i < packingList.length){
   console.log(packingList[i]);
+  i++;
 }
-console.log(packingList[3]);
-/* If we run this code, we can see that the program reminds us to pack all the items in our kitchen.
-Looking more deeply into the code,
-this works because we're declaring a counter variable called i (short for "index")
-that starts at 0, and increments by 1 at every step of a loop until
-it matches the length of our array, that is packingList. */
